6a6a0e72362913f9455e3ad7e2a3e7baf32aa018
Emerson Ribeiro <lyncoln_erc@hotmail.com>
2024-04-26 20:21:50 -0300

    criando Lista de Tarefas

---
+++ src/main/java/list/operacoesBasicas/ListaTarefa.java
@@ -0,0 +1,41 @@
+package main.java.list.operacoesBasicas;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class ListaTarefa {
+    //atributo
+    private List<Tarefa> tarefaList;
+
+    //construtor, sempre que for criada, vai ser criada um ArrayList vazia
+    public ListaTarefa() {
+        this.tarefaList = new ArrayList<>();
+    }
+
+    //métodos
+    public void adicionarTarefa(String descricao) {
+        tarefaList.add(new Tarefa(descricao));
+    }
+
+    public void removerTarefa(String descricao) {
+
+        List<Tarefa> tarefasParaRemover = new ArrayList<>();
+
+        for (Tarefa t : tarefaList) {
+            if (t.getDescricao().equalsIgnoreCase(descricao)) {
+                tarefasParaRemover.add(t);
+            }
+        }
+        tarefaList.removeAll(tarefasParaRemover);
+    }
+
+    public int obterNumeroTotalTarefas() {
+        return tarefaList.size();
+    }
+
+    public void obterDescricoesTarefas() {
+        System.out.println(tarefaList);
+    }
+
+
+}
---
+++ src/main/java/list/operacoesBasicas/Tarefa.java
@@ -0,0 +1,17 @@
+package main.java.list.operacoesBasicas;
+
+public class Tarefa {
+    //atributo
+    private String descricao;
+    public int idade;
+
+    public Tarefa(String descricao) {
+        this.descricao = descricao;
+    }
+    public String getDescricao() {
+        return this.descricao;
+    };
+
+
+
+}
